b71c27377d1d3c566863591e280d186949ebeadb
cleung0112 <cleung0112@gmail.com>
2021-02-27 22:21:18 -0800

    Create the base components for  product detail page

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 import styles from './style.css';
+import API from '../../../../config';
+import Category from './Category/Category.jsx';
 
 class Overview extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+      desciprtion: '',
+    };
+    this.getProductInfo = this.getProductInfo.bind(this);
+  }
+
+  getProductInfo() {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/14931', {
+      headers:
+        { Authorization: `${API}` },
+    }).then((data) => {
+      this.setState({
+        title: data.data.name,
+        category: data.data.category,
+        desciprtion: data.data.description,
+      });
+    })
+      .catch((err) => console.log('Err', err));
+  }
+
+  componentDidMount() {
+    this.getProductInfo();
   }
 
   render() {
     return (
       <div>
+        <Category category={this.state.category} />
         <h1 className={styles.title}>Overview </h1>
       </div>
-    )
+    );
   }
 }
-
 
 export default Overview;
