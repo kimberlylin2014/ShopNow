749a9b0faaeae2d6abd9986867fd224e592bca8b
Maggie Aytac <maggie.aytac@gmail.com>
2021-03-03 21:10:07 -0800

    cleaned up stars component

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import styles from './starsStyle.css';
 
 const Stars = ({ numStars }) => {
-  //example: numStars = 3.64
-  let numFilledStars = Math.floor(numStars); // 3
-  let decimal = Number((numStars - numFilledStars).toFixed(2)) * 100; // 64
-  let numQuarters = Math.round(decimal / 25); // 3
+  const numFilledStars = Math.floor(numStars);
+  const decimal = Number((numStars - numFilledStars).toFixed(2)) * 100;
+  const numQuarters = Math.round(decimal / 25);
   let numEmptyStars = 0;
   const array = [];
   for (let i = 0; i < numFilledStars; i++) {
@@ -26,23 +25,10 @@ const Stars = ({ numStars }) => {
   for (let j = 0; j < numEmptyStars; j++) {
     array.push('stars/emptyStar.png');
   }
-  // display {numFilledStars} filled stars
-  // numQuarters === 0 ? display {numStars - numFilledStars} empty stars
-  // numQuarters === 1 ? display quarter star and {numStars - numFilledStars - 1} empty stars
-  // numQuarters === 2 ? display half star and {numStars - numFilledStars - 1} empty stars
-  // numQuarters === 3 ? display three quarter star and {numStars - numFilledStars - 1} empty stars
-
-  // create array with five file names
-  // example: 3.64 would translate to this array:
-  // ['stars/filledStar.svg', 'stars/filledStar.svg', 'stars/filledStar.svg', 'stars/threeQuarterStar.svg', 'stars/emptyStar.svg']
 
   return (
     <div>
-      {array.map((starFile) => {
-        console.log(starFile);
-        return <img className={styles.star} src={`${starFile}`} alt={starFile} />
-      })}
-
+      {array.map((starFile) => <img className={styles.star} src={`${starFile}`} alt={starFile} />)}
     </div>
   );
 };
