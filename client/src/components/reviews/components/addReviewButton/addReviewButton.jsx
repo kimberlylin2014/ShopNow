9fa7cc56873ccdb15914e5acfef1098c8ca0dafc
Kimberly Lin <kimberlylin2014@gmail.com>
2021-03-05 15:08:56 -0800

    edit addReviewButton text

--- client/src/components/reviews/components/addReviewButton/addReviewButton.jsx
+++ client/src/components/reviews/components/addReviewButton/addReviewButton.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
 import styles from './addReviewButton.css';
-// class AddReviewButton extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick() {
-//     const { toggleFormDisplay } = this.props;
-//     toggleFormDisplay();
-//   }
-
-//   render() {
-//     return (
-//       <button onClick={this.handleClick} className={styles.buttonStyle}>ADD A REVIEW</button>
-//     );
-//   }
-// };
 
 const AddReviewButton = ({handleOpenModalButtonClick}) => (
   <button
     type="button"
     className={styles.buttonStyle}
-    onClick={handleOpenModalButtonClick}>
-    ADD A REVIEWS
+    onClick={handleOpenModalButtonClick}
+  >
+    ADD A REVIEW
   </button>
 );
-
 
 export default AddReviewButton;
