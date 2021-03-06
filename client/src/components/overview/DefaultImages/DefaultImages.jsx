d84f7ab52092c942eb91baebbb8d2e3c579eb76e
cleung0112 <cleung0112@gmail.com>
2021-03-05 16:22:19 -0800

    Add image carousel

--- client/src/components/overview/DefaultImages/DefaultImages.jsx
+++ client/src/components/overview/DefaultImages/DefaultImages.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import styles from './DefaultImages.css';
+import Styles from './DefaultImages.css';
+import ImageSlider from './ImageGallery/ImageGallery.jsx';
 
 const DefaultImages = ({
   photos, alt, key, allStyles,
 }) => {
-  console.log(photos, allStyles);
+
+  console.log(photos);
   if (photos) {
-    const displayImages = photos.map((pic, index) => <img className={styles.mainImage} src={pic.url} alt={alt} key={index} />);
-    return <div>{ displayImages }</div>;
+    const displayImages = photos.map((pic, index) => (
+      <img
+        className={Styles.mainImage}
+        src={pic.url}
+        alt={alt}
+        key={index}
+      />
+    ));
+
+    return (
+      <div className={Styles.gallery}>
+        {/* { displayImages } */}
+        <ImageSlider productImages={photos} />
+      </div>
+    );
   }
   return <div />;
 };
---
+++ client/src/components/overview/DefaultImages/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Styles from './ImageGallery.css';
+
+const ImageSlider = ({ productImages }) => {
+  const [current, setCurrent] = useState(0);
+  const { length } = productImages;
+
+  if (!Array.isArray(productImages) || length <= 0) {
+    return null;
+  }
+
+  const nextPic = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const previousPic = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  const imageGallery = productImages.map((image, index) => (
+    <div className={current === index ? Styles.CurrentPic : Styles.slider}>
+      { current === index && (
+        <img className={Styles.images} src={image.url} alt="productThumbnail" />)}
+    </div>
+  ));
+
+  return (
+    <div className={Styles.slider}>
+      <div className={Styles.LeftArrow} onClick={previousPic}>
+        <img src='icons/leftArrow.png' />
+      </div>
+      <div className={Styles.RightArrow} onClick={nextPic}>
+        <img src='icons/rightArrow.png' />
+      </div>
+      {imageGallery}
+    </div>
+  );
+};
+
+export default ImageSlider;
