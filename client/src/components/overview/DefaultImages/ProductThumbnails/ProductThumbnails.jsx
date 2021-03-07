3e76895dc9edae5132d6b92bedeccbec01ebd027
cleung0112 <cleung0112@gmail.com>
2021-03-06 16:04:02 -0800

    Add scroll feature to product thumbnails

--- client/src/components/overview/DefaultImages/ProductThumbnails/ProductThumbnails.jsx
+++ client/src/components/overview/DefaultImages/ProductThumbnails/ProductThumbnails.jsx
@@ -2,35 +2,45 @@ import React, { useState } from 'react';
 import Styles from './ProductThumbnails.css';
 
 const ProductThumbnails = ({ productThumbnails, currIndex, updateIndex }) => {
-  // const [currThumbnail, setCurrThumbnail] = useState(0);
-  const changeIndex = (e) => {
-    updateIndex(Number(e.target.getAttribute('index')));
-  };
+  const [tracker, settracker] = useState(0);
+  const { length } = productThumbnails;
 
-  const displayAllThumbnails = productThumbnails.map((thumbnail, index) => {
-    return (
-      <img
-        src={thumbnail.thumbnail_url}
-        alt="produc thumbnail"
-        index={index}
-        className={Styles.productThumbnail}
-        id={currIndex === index ? Styles.Selected : ''}
-        onClick={changeIndex}
-      />
-    );
-  });
+  const createThumbnail = (tracker) => {
+    if (tracker >= 0 && tracker < length) {
+      return (
+        <img
+          src={productThumbnails[tracker].thumbnail_url}
+          alt="product thumbnail"
+          className={Styles.productThumbnail}
+          id={currIndex === tracker ? Styles.Selected : ''}
+          onClick={() => updateIndex(tracker)}
+        />
+      );
+    }
+  };
 
   return (
     <div className={Styles.thumbnails}>
-      {displayAllThumbnails}
+      { productThumbnails.length > 0 && (
+      <div className={Styles.thumbnails}>
+        <button onClick={() => settracker(tracker === 0 ? 0 : tracker - 1)}>UP</button>
+        { createThumbnail(tracker) }
+        { createThumbnail(tracker + 1) }
+        { createThumbnail(tracker + 2) }
+        { createThumbnail(tracker + 3) }
+        { createThumbnail(tracker + 4) }
+        { createThumbnail(tracker + 5) }
+        { createThumbnail(tracker + 6) }
+        <button onClick={() => settracker( tracker + 7 === length ? tracker : tracker + 1)}> DOWN</button>
+      </div>
+      )}
     </div>
   );
 };
-// render all image thumbnails
-// change styling based on index received from parent
-// imageGallery send back current index to parent
-  // this is passed down to imgaethumbnails
-// when user clicks on new img thumbnails
-// passed back index to parent
+
+// if productThumbnails.length > 7, then set up a placeholder
+// for 7 thumbnails, and a down arrow
+// when down arrow is clicked, change the IndexState to 7
+// make up arrow appears
 
 export default ProductThumbnails;
