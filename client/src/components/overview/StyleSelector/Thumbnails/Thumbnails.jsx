b5bccd854b837840f60c4dcb51e1663231c7484a
cleung0112 <cleung0112@gmail.com>
2021-03-06 18:32:47 -0800

    Delete unncessary comment

--- client/src/components/overview/StyleSelector/Thumbnails/Thumbnails.jsx
+++ client/src/components/overview/StyleSelector/Thumbnails/Thumbnails.jsx
@@ -4,23 +4,9 @@ import Thumbnail from './Thumbnail/Thumbnail.jsx';
 import Styles from './Thumbnails.css';
 
 const Thumbnails = ({ index, photos, name, styleId, selected, updateStyleId, imageThumbnail, currentStyle}) => {
-  // console.log(photos);
-  // const displayThumbnail = photos.map((thumbnail) => (
-  //    <Thumbnail
-  //     currentStyle={currentStyle}
-  //     imageThumbnail={imageThumbnail}
-  //     index={index}
-  //     selected={selected}
-  //     url={thumbnail.thumbnail_url}
-  //     alt={name}
-  //     styleId={styleId}
-  //     key={thumbnail.thumbnail_url}
-  //     updateStyleId={updateStyleId}
-  //   />
-  // ));
+
   return (
     <div>
-      {/* {displayThumbnail} */}
       <Thumbnail
       currentStyle={currentStyle}
       imageThumbnail={imageThumbnail}
