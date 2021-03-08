8350d42d833f3da1e61f7366a19bcedbb8a54217
Kimberly Lin <kimberlylin2014@gmail.com>
2021-03-07 23:43:51 -0800

    removed checkmark in iRecommend when user does not recommend

--- client/src/components/reviews/components/iRecommend/iRecommend.jsx
+++ client/src/components/reviews/components/iRecommend/iRecommend.jsx
@@ -3,11 +3,14 @@ import styles from './iRecommend.css';
 
 const IRecommend = ({review}) => (
   <div className={styles.iRecommend}>
-    <img
+    {review.recommend ? (
+      <img
       className={styles.iRecommendImg}
       src="/icons/check.png"
       alt="green-checkmark"
-    />
+      />
+    ): null}
+
     <span className={styles.iRecommendText}>{review.recommend ? 'I recommend this product' : null }</span>
   </div>
 );
