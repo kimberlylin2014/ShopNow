280a92b2d0290c7b4900d5767a66bcf7c4c1cc93
cleung0112 <cleung0112@gmail.com>
2021-03-08 10:18:43 -0800

    Add indicators

---
+++ client/src/components/overview/DefaultImages/Modal/Indicators.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Styles from './Modal.css';
+
+const Indicators = ({currIndex, updateIndex, length}) => {
+
+  const makeIndicators = 
+
+
+  return length > 0 && (
+    <div className={Styles.Indicators}>
+
+    </div>
+  );
+};
+
+export default Indicators;
