d9de1d1b76b3fae7a9e7cb8d5f3f746063e553d7
cleung0112 <cleung0112@gmail.com>
2021-03-09 15:18:19 -0800

    Make default image bigger when device width is 1200px +

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,8 +4,10 @@ import Styles from './Header.css';
 const Header = ({ itemCount }) => (
   <div className={Styles.header}>
     <h1 className={Styles.storeName}>Holistic Hawks</h1>
+    <div className={Styles.shoppingbag}>
     <img className={Styles.cart} src="icons/shopping-bag.svg" alt="Logo" />
     <p className={Styles.itemCount}>{itemCount}</p>
+    </div>
   </div>
 );
 
