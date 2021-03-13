0a3f9d505b509d63f359d85ba50af81e707fad5c
cleung0112 <cleung0112@gmail.com>
2021-03-12 23:17:24 -0800

    Refactor style selector

--- client/src/components/overview/StyleSelector/StyleSelector.jsx
+++ client/src/components/overview/StyleSelector/StyleSelector.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import Thumbnails from './Thumbnails/Thumbnails.jsx';
+import Thumbnail from './Thumbnail/Thumbnail.jsx';
 import Styles from './StyleSelector.css';
 
-class StyleSelector extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const StyleSelector = ({ styles, currentStyle, updateStyleId }) => {
+  const displayThumbnails = styles.map((style) => (
+    <Thumbnail
+      key={style.photos[0].thumbnail_url}
+      url={style.photos[0].thumbnail_url}
+      currentStyle={currentStyle}
+      styleId={style.style_id}
+      updateStyleId={updateStyleId}
+      index={styles.indexOf(style)}
+      selected={currentStyle}
+      alt={style.name}
+    />
+  ));
 
-    render() {
-    const displayThumbnails = this.props.styles.map((style, index) => (
-      <Thumbnails
-        key={index}
-        imageThumbnail = {this.props.imageThumbnail}
-        currentStyle = {this.props.currentStyle}
-        styleId={style.style_id}
-        updateStyleId={this.props.updateStyleId}
-        index={this.props.styles.indexOf(style)}
-        selected={this.props.currentStyle}
-        photos={style.photos}
-        name={style.name}
-      />
-    ));
-
-    return (
-      <div className={Styles.thumbnails}>
-        {displayThumbnails}
-      </div>
-    );
-  }
-}
-
+  return (
+    <div className={Styles.thumbnails}>
+      {displayThumbnails}
+    </div>
+  );
+};
 
 export default StyleSelector;
